22ededfe7502c6dfc3c3333c970f8807b3adb95c
mabelth <makunko.love@gmail.com>
2021-01-06 19:28:56 -0600

    fgg

---
+++ dato_coneccion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+  <head>
+    <meta charset="utf-8">
+    <title>conecion</title>
+  </head>
+  <body>
+    <?php
+    
+    $db_host="localhost";
+    $db_nom="prueba";
+    $db_usuario="root";
+    $db_contraseña="";
+    
+    $conexion=mysqli_connect($db_host, $db_usuario,$db_contraseña,$db_nom,);
+    $consulta="selec *from datos";
+    $resultado=mysqli_query($conexion,$consulta);  
+    
+    ?>
+  </body>
+</html>
