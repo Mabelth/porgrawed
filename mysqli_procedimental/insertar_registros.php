9d32913aa2327314a7636f9eea0a305677e3bddc
mabelth <makunko.love@gmail.com>
2021-02-02 16:54:37 -0600

    se guardo

---
+++ mysqli_procedimental/insertar_registros.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+
+<head>
+    <meta charset="utf-8">
+    <title>busqueda_resultado</title>
+
+</head>
+
+<body>
+    <?php
+        require("datos.php");
+        $nom=$_GET["nom"];
+        $apell = $_GET["apell"];
+        $edad = $_GET["edad"];
+    
+
+        $conexion = mysqli_connect($db_host, $db_usuario, $db_contraseña, $db_nom);
+
+        if (mysqli_connect_errno()) {
+            echo "fallo en bd";
+            exit();
+        } else {
+            echo " SE CONECTO";
+        }
+        mysqli_select_db($conexion, $db_nom) or die("no se encuentra la bd");
+        mysqli_set_charset($conexion, "utf8");
+        $consulta = "INSERT INTO datos ( nom, apell, edad) VALUES ( '$nom', '$apell', '$edad');";
+        $resultado = mysqli_query($conexion, $consulta);
+        if($resultado==false)
+        { echo"error en la consuta";}
+        else
+        {echo "registro guardado <br><br>";
+        echo "<table><tr><td>";
+        echo $fila['id'] . "</td><td>";
+        echo $fila['nom'] . "</td><td>";
+        echo $fila['apell'] . "</td><td>";
+        echo $fila['edad'] . "</td></tr></table>";
+        echo "<br>";
+        
+        }
+       
+      mysqli_close(exit);
+    ?>
+
+</body>
+
+</html>
