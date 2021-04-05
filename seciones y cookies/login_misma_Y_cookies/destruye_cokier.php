692a464cf9990281395d24d9e8072d9db3d06dc8
mabelth <makunko.love@gmail.com>
2021-04-05 15:36:02 -0500

    casi listo

---
+++ seciones y cookies/login_misma_Y_cookies/destruye_cokier.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+
+<head>
+    <meta charset="utf-8">
+    <title>cokies destruri</title>
+</head>
+
+<?php
+setcookie("borrar", "Uki", time()-1);
+echo "cookie borrada";
+?>
+
+</html>
